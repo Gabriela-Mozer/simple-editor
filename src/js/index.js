9b368a9ff49c84fd224c9c67a18c88ee8a772109
Gabriela Mozer <gabrielamozer1966@gmail.com>
2021-01-06 20:16:51 +0100

    adding button to change bc color

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,35 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+document.cookie='name = gabi'
+
+
+
+const project ={
+    name: 'Simple Editor',
+    include: 'textarea and buttons',
+    createTo: 'save changes'
+
+}
+console.log(project);
+
+const box = 'open !'
+console.log(`${box}`);
+
+let isDark = false;
+
+const switchModes = document.querySelector('.backgroundColor');
+switchModes.addEventListener('click',() => {
+    if(isDark){
+        document.documentElement.style.setProperty('--background-color', 'rgb(181, 131, 141)')
+        isDark=false;
+    }
+    else{
+        document.documentElement.style.setProperty('--background-color', 'rgb(109, 104, 117)')
+        isDark=true;
+    }
+});  
+//     localStorage.setItem('entry',entryTextarea.value);
+// });
